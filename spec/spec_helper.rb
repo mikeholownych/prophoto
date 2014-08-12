7ea7c631e1b04802cb39cc60596b63bd57889cce
Mike Holownych <mike.holownych@gmail.com>
2014-08-12 01:11:47 +0000

    Setup Invitations and SimpleForm

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,15 +4,18 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'database_cleaner'
 require 'capybara/rspec'
+require 'email_spec'
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
-Capybara.app_host = 'http://example.com'
-Capybara.default_host = 'http://example.com'
+Capybara.app_host = 'http://lvh.com'
+Capybara.default_host = 'http://lvh.com'
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+  config.include EmailSpec::Helpers
+  config.include EmialSpec::Matchers
   config.include Rails.application.routes.url_helpers
   config.include FactoryGirl::Syntax::Methods
   config.include Capybara::DSL
@@ -32,5 +35,7 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
     Apartment::Database.reset
     drop_schemas
+    Capybara.app_host = 'http://example.com'
+    reset_mailer
   end
 end
